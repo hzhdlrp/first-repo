e75fb92c6905f84b3d39e05f2a142c6a739b0355
hzhdlrp <pswjft@gmail.com>
2024-01-31 21:28:42 +0300

    ?

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     LongNum num1("10");
     LongNum num2( "6.257475467928");
     LongNum num3("43654.03845793757350253");
-    LongNum num4 = num2 + num3;
+    LongNum num4 = num2 * num3;
     std::cout << (num4);
 
     clock_t end = clock();
