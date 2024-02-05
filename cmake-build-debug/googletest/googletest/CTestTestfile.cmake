d0d9c65e9974d4f7705f6896965d0a7040ac3a2e
hzhdlrp <pswjft@gmail.com>
2024-02-06 01:26:45 +0300

    ?

---
+++ cmake-build-debug/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vadimleonov/CLionProjects/project1/googletest/googletest
+# Build directory: /Users/vadimleonov/CLionProjects/project1/cmake-build-debug/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
